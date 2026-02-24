2d45e0fdaa81028fadab29a8fcafda4c54e07dcb
luanmxz <luanmarcenemxz22@gmail.com>
2026-02-24 19:37:44 -0300

    CHORE(spring) - Spring Initializer

---
+++ src/main/java/com/nimblesoftwares/ttrilha_api/TtrilhaApiApplication.java
@@ -0,0 +1,13 @@
+package com.nimblesoftwares.ttrilha_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtrilhaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtrilhaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimblesoftwares/ttrilha_api/TtrilhaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimblesoftwares.ttrilha_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtrilhaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
