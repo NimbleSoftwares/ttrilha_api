4876ea71205d7adf386ad3a249deb09d0719a420
luanmxz <luanmarcenemxz22@gmail.com>
2026-02-25 01:56:13 -0300

    feat(auth): implement JWT resource server with audience validation
    - Added SecurityConfig to configure OAuth2 Resource Server with JWT validation.
    - Implemented AudienceValidator for custom audience verification.
    - Configured issuer-uri and audience properties in application YAML files.
    - Introduced a /api/v1/test-jwt endpoint to test JWT authentication.
    
    Signed-off-by: luanmxz <luanmarcenemxz22@gmail.com>

--- src/main/java/com/nimblesoftwares/ttrilha_api/config/SecurityConfig.java
+++ src/main/java/com/nimblesoftwares/ttrilha_api/config/SecurityConfig.java
@@ -1,22 +1,48 @@
 package com.nimblesoftwares.ttrilha_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+  private String issuerUri;
+
+  @Value("${auth0.audience}")
+  private String audience;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       return http
           .csrf(CsrfConfigurer::disable)
+          .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
           .authorizeHttpRequests(authorize -> authorize
-              .requestMatchers("/api/v1/auth/**").permitAll()
               .requestMatchers("/actuator/**").permitAll()
               .anyRequest().authenticated())
+          .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
           .build();
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(issuerUri);
+
+    OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
+    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+    OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+    jwtDecoder.setJwtValidator(withAudience);
+
+    return jwtDecoder;
   }
 }
