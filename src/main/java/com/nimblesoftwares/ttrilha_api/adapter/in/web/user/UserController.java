cd68bd8bbce0af248550f703dc6343dfb0a09f04
luanmxz <luanmarcenemxz22@gmail.com>
2026-02-28 15:21:03 -0300

    feat(api): Initiating integrate Resilience4j and expose circuit breaker endpoint
    - Added `resilience4j-spring-boot3` dependency for resilience patterns.
    - Configured Circuit Breaker (`test-circuit-breaker`) and Rate Limiter instances in `application.yaml`.
    - Updated `application-test.yaml` to include management endpoint exposure for health, info, and circuit breakers.
    - Introduced `/test-circuit` endpoint in `UserController` with Circuit Breaker annotation for testing.
    - Enhanced fault tolerance capabilities across the API.
    
    Signed-off-by: luanmxz <luanmarcenemxz22@gmail.com>

--- src/main/java/com/nimblesoftwares/ttrilha_api/adapter/in/web/user/UserController.java
+++ src/main/java/com/nimblesoftwares/ttrilha_api/adapter/in/web/user/UserController.java
@@ -4,6 +4,7 @@ import com.nimblesoftwares.ttrilha_api.adapter.in.web.user.dto.SaveUserRequest;
 import com.nimblesoftwares.ttrilha_api.adapter.in.web.user.dto.SaveUserResponse;
 import com.nimblesoftwares.ttrilha_api.application.user.command.SaveUserCommand;
 import com.nimblesoftwares.ttrilha_api.application.user.port.in.SaveUserUseCase;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,10 +16,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -68,5 +66,12 @@ public class UserController {
         .toUri();
 
     return ResponseEntity.created(location).body(response);
+  }
+
+
+  @GetMapping(value = "/test-circuit")
+  @CircuitBreaker(name = "test-circuit-breaker")
+  public ResponseEntity<String> testCircuit() {
+    throw new RuntimeException("Simulating circuit breaker failure");
   }
 }
